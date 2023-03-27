1543fa54db89f00ab8b08b3b37173ca82dc77649
Vadim Piryazev <v_a_d@inbox.ru>
2023-03-27 21:59:09 +0300

    update component index file

--- components/index.js
+++ components/index.js
@@ -1,4 +1,6 @@
 import HelloWorld from './HelloWorld';
 import CoreComponents from './CoreComponents';
+import Cat from './Cat';
+import Cafe from './Cafe';
 
-export {HelloWorld, CoreComponents};
+export {HelloWorld, CoreComponents, Cat, Cafe};
