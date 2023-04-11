01905e9f5b4b41ac17ab2a05eadc490b6529f53e
Vadim Piryazev <v_a_d@inbox.ru>
2023-04-11 14:27:38 +0300

    update output

--- components/Cafe.tsx
+++ components/Cafe.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {Cat, FishTranslator, SectionListBasic} from './index';
+// import {View, Text, StyleSheet} from 'react-native';
+import {ButtonExemples} from './index';
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   cafeTitleTextStyles: {
     fontWeight: 'bold',
     fontSize: 20,
   },
-});
+}); */
 
 const Cafe = () => {
-  return (
-    <View>
-      <Text style={styles.cafeTitleTextStyles}>Наши котики в кафе:</Text>
-      <Cat
-        name="Vaska"
-        source={{
-          uri: 'https://natalyland.ru/wp-content/uploads/1/1/8/118c2c4bf1e1106f57d640e22759b3cd.jpeg',
-        }}
-        // eslint-disable-next-line react-native/no-inline-styles
-        size={{height: 300, width: 'auto'}}
-      />
-      <FishTranslator />
-      <SectionListBasic />
-    </View>
-  );
+  return <ButtonExemples />;
 };
 
 export default Cafe;
